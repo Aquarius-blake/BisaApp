dc5d5a5fdf194f4ca3a427efcba65e533957a0f8
Aquarius-blake <danbaid72@gmail.com>
2024-04-02 17:21:43 +0000

    update Fitness_Profile.dart and Set_fitness_goals.dart

--- lib/ui/home/Fitness/Set_fitness_goals.dart
+++ lib/ui/home/Fitness/Set_fitness_goals.dart
@@ -1,6 +1,11 @@
 
 
+import 'package:bisa_app/models/current_user.dart';
+import 'package:bisa_app/providers/current_user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FitnessGoals extends StatefulWidget {
   const FitnessGoals({super.key});
@@ -10,8 +15,75 @@ class FitnessGoals extends StatefulWidget {
 }
 
 class _FitnessGoalsState extends State<FitnessGoals> {
+
+
+   late SharedPreferences prefs;
+  late CurrentUser currentUser;
+  String? gender;
+
+
+@override
+  void initState() {
+    initialize();
+    super.initState();
+  }
+
+initialize()async{
+  prefs = await SharedPreferences.getInstance();
+  gender = await prefs.getString("gender") ?? "Male";
+  if(mounted){
+    setState(() {
+      
+    });
+  }
+}
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    currentUser = context.read<CurrentUserProvider>().currentUser!;
+    return Theme(
+      data: ThemeData(
+        primarySwatch: gender == "Male" ?Colors.amber : Colors.pink,
+      ),
+      child: Container(
+        decoration:  BoxDecoration(
+          image: DecorationImage(
+            image: gender == "Male" ? const AssetImage("assets/workout/test.png"): const AssetImage("assets/workout/test2.png"),
+            fit: BoxFit.cover,
+            colorFilter: const ColorFilter.mode(Colors.black54, BlendMode.darken)
+          )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          extendBody: true,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.transparent,
+                  height:250.h,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height*0.9,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30)
+                        )
+                      ),
+                      child: Column(
+                        children: [],
+                      ),
+                )
+              ],
+            ),
+          ),
+        )
+        ),
+    );
   }
 }
