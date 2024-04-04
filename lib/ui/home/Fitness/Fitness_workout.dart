7b358a951a1df7c54e5c77aa44bbe4e5c84c2361
Aquarius-blake <danbaid72@gmail.com>
2024-04-04 16:31:41 +0000

    update 8 files

--- lib/ui/home/Fitness/Fitness_workout.dart
+++ lib/ui/home/Fitness/Fitness_workout.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class WorkoutPage extends StatefulWidget {
   const WorkoutPage({super.key});
@@ -9,10 +10,40 @@ class WorkoutPage extends StatefulWidget {
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
+  final _player1 = AudioPlayer();
+  final _player2 = AudioPlayer();
+
+
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+
+
+  Future<void> _setupAudioPlayers()async{
+    _player1.playbackEventStream.listen((event) { },
+    onError: (Object e, StackTrace stacktrace){
+      print("A Stream error occured: ${e.toString()}");
+    }
+  );
+  _player2.playbackEventStream.listen((event) { },
+    onError: (Object e, StackTrace stacktrace){
+      print("A Stream error occured: ${e.toString()}");
+    }
+  );
+  try{
+    _player1.setAudioSource(AudioSource.asset('assets/workout/nextset.mp3'));
+     _player2.setAudioSource(AudioSource.asset('assets/workout/break.wav'));
+  }catch(e){
+     print("Error Loading audio source: ${e.toString()}"); 
+  }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
     );
   }
 }
