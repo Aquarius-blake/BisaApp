7f7312fd3870d93c2a4a8ba788dcb3339ee9bf72
Aquarius-blake <danbaid72@gmail.com>
2024-04-15 11:45:11 +0000

    update 8 files

---
+++ lib/ui/home/FemaleCycle/CycleResult.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class CycleResult extends StatefulWidget {
+  const CycleResult({super.key});
+
+  @override
+  State<CycleResult> createState() => _CycleResultState();
+}
+
+class _CycleResultState extends State<CycleResult> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/ui/home/FemaleCycle/Cyclehome.dart
+++ lib/ui/home/FemaleCycle/Cyclehome.dart
@@ -1,7 +1,8 @@
 
 
-
+import 'package:bisa_app/animation/fade_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CycleHome extends StatefulWidget {
   const CycleHome({super.key});
@@ -11,8 +12,56 @@ class CycleHome extends StatefulWidget {
 }
 
 class _CycleHomeState extends State<CycleHome> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 150,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(40),
+                    topLeft: Radius.circular(40)
+                  )
+                ),
+                child: Column(
+                  children: [
+                    FadeAnimation(
+                1.2,
+                0,
+                30,
+                Text(
+                  "Track your cycle",
+                  style: TextStyle(
+                     fontFamily: 'Poppins',
+                     fontSize: 20.sp,
+                     color:  Colors.pink,
+                     ),
+                  )
+                   ),
+                   SizedBox(height: 20,)
+                   
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
