2663fc1bf66d14cca567e14881c77f3452d4e240
Aquarius-blake <danbaid72@gmail.com>
2024-04-15 16:27:06 +0000

    update CycleResult.dart

--- lib/ui/home/FemaleCycle/CycleResult.dart
+++ lib/ui/home/FemaleCycle/CycleResult.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CycleResult extends StatefulWidget {
   final DateTime lastperiod;
@@ -15,6 +16,48 @@ class CycleResult extends StatefulWidget {
 class _CycleResultState extends State<CycleResult> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      //backgroundColor: Colors.pink[100],
+      appBar: AppBar(
+        toolbarHeight: 30,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new_outlined),
+          ),
+      ),
+      body: Container(
+        padding:const EdgeInsets.symmetric(
+          horizontal: 20
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TableCalendar(
+                focusedDay: widget.lastperiod, 
+                firstDay: DateTime(1800), 
+                lastDay: widget.lastperiod.add(const Duration(days: 365)),
+                rangeStartDay: widget.lastperiod,
+                rangeEndDay: widget.lastperiod.add(Duration(days: widget.period!)),
+                calendarStyle: CalendarStyle(
+                  rangeHighlightColor: Colors.pink[200]!,
+                  rangeStartDecoration: BoxDecoration(
+                    color: Colors.pink[300],
+                    shape: BoxShape.circle
+                  ),
+                  rangeEndDecoration: BoxDecoration(
+                    color: Colors.pink[300],
+                    shape: BoxShape.circle
+                  ),
+                ),
+                onFormatChanged: (value){},
+                )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
