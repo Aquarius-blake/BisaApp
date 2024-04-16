660ed7b5009e947e51177d1458d90e498f34acde
Aquarius-blake <danbaid72@gmail.com>
2024-04-16 11:24:55 +0000

    update 8 files

--- lib/ui/home/FemaleCycle/CycleResult.dart
+++ lib/ui/home/FemaleCycle/CycleResult.dart
@@ -1,6 +1,9 @@
 
 
+import 'package:bisa_app/animation/fade_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CycleResult extends StatefulWidget {
@@ -53,7 +56,131 @@ class _CycleResultState extends State<CycleResult> {
                   ),
                 ),
                 onFormatChanged: (value){},
-                )
+                ),
+                const SizedBox(height: 20,),
+                FadeAnimation(
+                1.2,
+                0,
+                30,
+                Text(
+                  "Estimated Period Range",
+                  style: TextStyle(
+                     fontFamily: 'Poppins',
+                     fontSize: 20.sp,
+                     color:  Colors.pink,
+                     ),
+                  )
+                   ),
+                   const SizedBox(height: 40,),
+                   FadeAnimation(
+                        1.2,
+                    0,
+                    30,
+                     Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                         children: [ Container(
+                             height: 100,
+                              width: 100,
+                              padding:const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: Colors.pink[300],
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                   BoxShadow(
+                                  color:Colors.pink[200]!,
+                                  spreadRadius: 9,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 0), // changes position of shadow
+                                ),
+                                ]
+                              ),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "${DateFormat('dd').format(widget.lastperiod.add(Duration(days:widget.cycle!)))} ",
+                                      style: TextStyle(
+                                       fontFamily: 'Poppins',
+                                       fontWeight: FontWeight.w600,
+                                       fontSize: 30.sp,
+                                       color:  Colors.white,
+                                       ),
+                                       textAlign: TextAlign.center,
+                                      ),
+                                      Text(
+                                      " ${DateFormat('MMMM').format(widget.lastperiod.add(Duration(days:widget.cycle!)))}",
+                                      style: TextStyle(
+                                       fontFamily: 'Poppins',
+                                       fontSize: 10.sp,
+                                       color:  Colors.white,
+                                       ),
+                                       textAlign: TextAlign.center,
+                                      ),
+                                  ],
+                                ),
+                              ),
+                           ),
+                           Container(
+                             height: 100,
+                              width: 100,
+                              padding:const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: Colors.pink[300],
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                   BoxShadow(
+                                  color:Colors.pink[200]!,
+                                  spreadRadius: 9,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 0), // changes position of shadow
+                                ),
+                                ]
+                              ),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "${DateFormat('dd').format(widget.lastperiod.add(Duration(days: widget.period!+ widget.cycle!)))} ",
+                                      style: TextStyle(
+                                       fontFamily: 'Poppins',
+                                       fontWeight: FontWeight.w600,
+                                       fontSize: 30.sp,
+                                       color:  Colors.white,
+                                       ),
+                                       textAlign: TextAlign.center,
+                                      ),
+                                      Text(
+                                      " ${DateFormat('MMMM').format(widget.lastperiod.add(Duration(days: widget.period!+ widget.cycle!)))}",
+                                      style: TextStyle(
+                                       fontFamily: 'Poppins',
+                                       fontSize: 10.sp,
+                                       color:  Colors.white,
+                                       ),
+                                       textAlign: TextAlign.center,
+                                      ),
+                                  ],
+                                ),
+                              ),
+                           ),
+                         ],
+                       ),
+                   ),
+                     const SizedBox(height: 30,),
+                     FadeAnimation(
+                1.2,
+                0,
+                30,
+                Text(
+                  "Estimated Possible Safe Days Range",
+                  style: TextStyle(
+                     fontFamily: 'Poppins',
+                     fontSize: 20.sp,
+                     color:  Colors.pink,
+                     ),
+                  )
+                   ),
             ],
           ),
         ),
