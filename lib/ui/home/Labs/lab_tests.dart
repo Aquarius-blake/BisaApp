98c47ff0793f050f9b062739767cacfa3bb1a9d5
Aquarius-blake <danbaid72@gmail.com>
2024-04-25 12:24:52 +0000

    update lab_tests.dart

--- lib/ui/home/Labs/lab_tests.dart
+++ lib/ui/home/Labs/lab_tests.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LabTest extends StatefulWidget {
   final String? lab;
@@ -12,6 +13,29 @@ class LabTest extends StatefulWidget {
 class _LabTestState extends State<LabTest> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(23, 30, 60, 1),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context); 
+          }, 
+          icon: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Colors.white,
+            )
+          ),
+        title: Text(
+         widget.lab ?? 'Lab',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontSize: 20.sp,
+          ),
+          ),
+         
+      ),
+    );
   }
 }
