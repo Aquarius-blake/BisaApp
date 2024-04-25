039ab9d526e83007c93155b6ec45111146a69582
Aquarius-blake <danbaid72@gmail.com>
2024-04-25 13:29:30 +0000

    i

---
+++ lib/ui/home/Home_routes/General.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class GeneralWellnes extends StatefulWidget {
+  const GeneralWellnes({super.key});
+
+  @override
+  State<GeneralWellnes> createState() => _GeneralWellnesState();
+}
+
+class _GeneralWellnesState extends State<GeneralWellnes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/ui/home/Home_routes/Physic.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class Physic extends StatefulWidget {
+  const Physic({super.key});
+
+  @override
+  State<Physic> createState() => _PhysicState();
+}
+
+class _PhysicState extends State<Physic> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
