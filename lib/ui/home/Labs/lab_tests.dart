f98f76962aa9c281280722a7109d7b3839d9d6f7
Aquarius-blake <danbaid72@gmail.com>
2024-04-25 12:17:41 +0000

    update 8 files

---
+++ lib/ui/home/Labs/lab_tests.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class LabTest extends StatefulWidget {
+  const LabTest({super.key});
+
+  @override
+  State<LabTest> createState() => _LabTestState();
+}
+
+class _LabTestState extends State<LabTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/ui/home/Labs/labs_home.dart
+++ lib/ui/home/Labs/labs_home.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LabsHome extends StatefulWidget {
   const LabsHome({super.key});
@@ -9,8 +10,79 @@ class LabsHome extends StatefulWidget {
 }
 
 class _LabsHomeState extends State<LabsHome> {
+
+
+List labs = [
+   "Lab A",
+    "Lab B",
+    "Lab C",
+    "Lab D",
+    "Lab E",
+    "Lab F",
+    "Lab G",
+];
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromRGBO(23, 30, 60, 1),
+        title: Text(
+          'Labs',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontSize: 20.sp,
+          ),
+          ),
+          centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 10
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height*0.8,
+              child: ListView.builder(
+                itemCount: labs.length,
+                itemBuilder: (context,index) => InkWell(
+                  onTap: (){
+                   // PageAnimateNoRep(context, PageTransitionType.rightToLeft, LabcyDrugs(Labcyname:Labcies[index],));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow:const <BoxShadow>[
+                        BoxShadow(
+                          color: Colors.grey,
+                          spreadRadius: 1,
+                          blurRadius: 2,
+                          offset:  Offset(2, 1), 
+                        )
+                      ]
+                    ),
+                    child: Row(
+                      children: [
+                        Text(labs[index]),
+                      ],
+                    ),
+                  ),
+                )
+                ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
