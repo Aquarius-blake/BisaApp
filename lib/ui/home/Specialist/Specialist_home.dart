08ae47ec77e79c04a6f234b2f37f7f47022b4899
Aquarius-blake <danbaid72@gmail.com>
2024-04-29 10:49:03 +0000

    update 8 files

--- lib/ui/home/Specialist/Specialist_home.dart
+++ lib/ui/home/Specialist/Specialist_home.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SpecialistHome extends StatefulWidget {
   const SpecialistHome({super.key});
@@ -12,6 +13,28 @@ class SpecialistHome extends StatefulWidget {
 class _SpecialistHomeState extends State<SpecialistHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Colors.white,
+            ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: const Color.fromRGBO(23, 30, 60, 1),
+        title: Text(
+          'Specialists',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontSize: 20.sp,
+          ),
+          ),
+          centerTitle: true,
+      ),
+    );
   }
 }
