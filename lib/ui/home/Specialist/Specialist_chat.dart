4b4705e12922855b9e657d66b8cb721e596d80fd
Aquarius-blake <danbaid72@gmail.com>
2024-07-27 23:02:08 +0000

    update Specialist_chat.dart

--- lib/ui/home/Specialist/Specialist_chat.dart
+++ lib/ui/home/Specialist/Specialist_chat.dart
@@ -12,8 +12,25 @@ class SpecialistChat extends StatefulWidget {
 class _SpecialistChatState extends State<SpecialistChat> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return Container(
+       padding: const EdgeInsets.only(
+        top:0
+      ),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+           begin: AlignmentDirectional.topCenter,
+           end: Alignment.bottomCenter,
+          colors: [
+            Colors.white,
+            Color.fromARGB(255, 247, 245, 245),
+            Color.fromARGB(255, 232, 232, 232),
+            Color.fromARGB(255, 232, 232, 232)
+          ]
+          )
+      ),
+      child: Scaffold(
+        body: Container(),
+      ),
     );
   }
 }
