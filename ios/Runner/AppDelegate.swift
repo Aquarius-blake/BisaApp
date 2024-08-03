a271071129ff8b4136e7e98e7e0b7263cf8438d8
Aquarius-blake <danbaid72@gmail.com>
2024-08-03 01:30:41 +0000

    update .gitignore, AndroidManifest.xml and AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // GMSServices.provideAPIKey("AIzaSyBqTw3akjU0-dv6aOowMc84yWprUhJepWY")
+    //Add google map api key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
